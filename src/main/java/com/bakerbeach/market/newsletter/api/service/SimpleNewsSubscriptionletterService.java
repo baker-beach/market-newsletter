51a1709fa3e9995c7844f65268fc420a026214fe
Achim Wolf <achim.wolf@windeln.de>
2016-12-31 10:00:55 +0100

    setup

---
+++ src/main/java/com/bakerbeach/market/newsletter/api/service/SimpleNewsSubscriptionletterService.java
@@ -0,0 +1,40 @@
+package com.bakerbeach.market.newsletter.api.service;
+
+import java.util.Date;
+
+import com.bakerbeach.market.newsletter.api.model.NewsletterSubscription;
+import com.bakerbeach.market.newsletter.api.service.NewsletterServiceException.AlreadySubscribedException;
+
+public class SimpleNewsSubscriptionletterService implements NewsletterSubscriptionService {
+	private SimpleNewsletterSubscriptionServiceDao newsletterDao;  
+
+	@Override
+	public void subscribe(String prefix, String firstName, String lastName, String email, String newsletterCode, String status) throws NewsletterServiceException {
+		NewsletterSubscription subscription = new NewsletterSubscription();
+		subscription.setPrefix(prefix);
+		subscription.setFirstName(firstName);
+		subscription.setLastName(lastName);
+		subscription.setEmail(email);
+		subscription.setNewsletterCode(newsletterCode);
+		subscription.setStatus(status);
+		subscription.setLastUpdate(new Date());
+		
+		// TOOD: check if already exists
+		// TODO: show global error message 
+		boolean alreadySubscribed = newsletterDao.update(subscription);
+		if (alreadySubscribed) {
+			throw new AlreadySubscribedException();
+		}
+	}
+	
+	@Override
+	public NewsletterSubscription getSubscription(String email) throws NewsletterServiceException {
+		NewsletterSubscription subscription = newsletterDao.findByEmail(email);
+		return subscription;
+	}
+
+	public void setNewsletterDao(SimpleNewsletterSubscriptionServiceDao newsletterDao) {
+		this.newsletterDao = newsletterDao;
+	}
+
+}
